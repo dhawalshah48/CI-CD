d3a43dfa9f0e1bd2b22ded2d0f991e9e9f8ec2bc
Dhawal Shah <dhawal.shah@avalara.com>
2020-04-18 00:07:21 +0530

    Done Task 1

---
+++ src/main/scala/Task1.scala
@@ -0,0 +1,37 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
+import akka.stream.ActorMaterializer
+
+
+object Task1 extends App {
+
+  implicit val system = ActorSystem("HttpService")
+  implicit val materializer = ActorMaterializer()
+
+  import akka.http.scaladsl.server.Directives._
+  import system.dispatcher
+
+  val welcomeRoute =
+    path("greet" / Segment) { (name: String) =>
+      complete(
+        StatusCodes.OK,
+        HttpEntity(
+          ContentTypes.`application/json`,
+          s"""{"message": "Hello $name"}""".stripMargin
+        )
+      )
+    } ~
+    path("health") {
+      complete(
+        StatusCodes.OK,
+        HttpEntity(
+          ContentTypes.`application/json`,
+          """{"status": "OK"}"""
+        )
+      )
+    }
+
+  Http().bindAndHandle(welcomeRoute, "localhost", 8080)
+
+}
