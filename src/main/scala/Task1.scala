748ccdfade7397a28054d3b88c9a9d53b6ad4c1c
Dhawal Shah <dhawal.shah@avalara.com>
2020-04-21 18:23:56 +0530

    seperated test file from object file

--- src/main/scala/Task1.scala
+++ src/main/scala/Task1.scala
@@ -2,31 +2,9 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
-import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.stream.ActorMaterializer
-import org.scalatest.{Matchers, WordSpec}
 
-class Task1 extends WordSpec with Matchers with ScalatestRouteTest {
-
-  import Task1.welcomeRoute
-
-  "Task1" should {
-    "return JSON with the name supplied" in {
-      Get("/greet/Dhawal") ~> welcomeRoute ~> check {
-        status shouldBe StatusCodes.OK
-        entityAs[String] shouldBe "{\"message\": \"Hello Dhawal\"}"
-      }
-    }
-    "return JSON of status" in {
-      Get("/health") ~> welcomeRoute ~> check {
-        status shouldBe StatusCodes.OK
-        entityAs[String] shouldBe "{\"status\": \"OK\"}"
-      }
-    }
-  }
-}
-
-object Task1 extends SprayJsonSupport{
+object Task1 extends SprayJsonSupport {
 
   implicit val system = ActorSystem("HttpService")
   implicit val materializer = ActorMaterializer()
---
+++ src/test/scala/Task1Test.scala
@@ -0,0 +1,23 @@
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.scalatest.{FunSuite, Matchers, WordSpec}
+
+class Task1Test extends WordSpec with Matchers with ScalatestRouteTest {
+
+  import Task1.welcomeRoute
+
+  "Task1" should {
+    "return JSON with the name supplied" in {
+      Get("/greet/Dhawal") ~> welcomeRoute ~> check {
+        status shouldBe StatusCodes.OK
+        entityAs[String] shouldBe "{\"message\": \"Hello Dhawal\"}"
+      }
+    }
+    "return JSON of status" in {
+      Get("/health") ~> welcomeRoute ~> check {
+        status shouldBe StatusCodes.OK
+        entityAs[String] shouldBe "{\"status\": \"OK\"}"
+      }
+    }
+  }
+}
