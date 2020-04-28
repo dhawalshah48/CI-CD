cc5adef264603e6d61a0fb61436a6f04052f8fa7
Dhawal Shah <dhawal.shah@avalara.com>
2020-04-28 23:34:18 +0530

    changed the spelling mistakes in deploy.sh file

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -3,8 +3,8 @@ SERVICE_PRESENT="$(kubectl describe services | grep -c "go-pipeline-service")"
 j2 ./ops/Kubernetes/deployment-defination.j2.yml > ./ops/Kubernetes/deployment-defination.yml
 if [ "$SERVICE_PRESENT" == 0 ]
 then
-  kubectl create -f ops/kubernetes/deployment-definition.yml --record
-  kubectl create -f ops/kubernetes/service-definition.yml
+  kubectl create -f ops/kubernetes/deployment-defination.yml --record
+  kubectl create -f ops/kubernetes/service-defination.yml
 else
-  kubectl apply -f ops/kubernetes/deployment-definition.yml --record
+  kubectl apply -f ops/kubernetes/deployment-defination.yml --record
 fi 
