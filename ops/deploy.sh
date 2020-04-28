4b82a21ac6230ee3bc67af14f84f60ee0661a92e
Dhawal Shah <dhawal.shah@avalara.com>
2020-04-28 22:55:23 +0530

    chnaged Kubernetes -> kubernetes and added if else in deploy.sh

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -1,3 +1,9 @@
+SERVICE_PRESENT="$(kubectl describe services | grep -c "go-pipeline-service")"
 j2 ./ops/Kubernetes/deployment-defination.j2.yml > ./ops/Kubernetes/deployment-defination.yml
-kubectl create -f ops/kubernetes/deployment-definition.yml --record
-kubectl create -f ops/kubernetes/service-definition.yml
+if [ "$SERVICE_PRESENT" == 0 ]
+then
+  kubectl create -f ops/kubernetes/deployment-definition.yml --record
+  kubectl create -f ops/kubernetes/service-definition.yml
+else
+  kubectl apply -f ops/kubernetes/deployment-definition.yml --record
+fi 
