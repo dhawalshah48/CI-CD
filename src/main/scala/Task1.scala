1ef2055cac88b688d8849bbe9efd7b9d8dc100d3
Dhawal Shah <dhawal.shah@avalara.com>
2020-04-28 10:14:48 +0530

    .

--- src/main/scala/Task1.scala
+++ src/main/scala/Task1.scala
@@ -2,37 +2,37 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
+import akka.http.scaladsl.server.Directives.{complete, path}
 import akka.stream.ActorMaterializer
 
 object Task1 extends SprayJsonSupport {
+  implicit val system = ActorSystem("HttpService")
+  implicit val materializer = ActorMaterializer()
 
-  def main(args: Array[String]): Unit = {
-    implicit val system = ActorSystem("HttpService")
-    implicit val materializer = ActorMaterializer()
+  import akka.http.scaladsl.server.Directives._
+  import system.dispatcher
 
-    import akka.http.scaladsl.server.Directives._
-    import system.dispatcher
-
-    val welcomeRoute =
-      path("greet" / Segment) { (name: String) =>
+  val welcomeRoute =
+    path("greet" / Segment) { (name: String) =>
+      complete(
+        StatusCodes.OK,
+        HttpEntity(
+          ContentTypes.`application/json`,
+          s"""{"message": "Hello $name"}""".stripMargin
+        )
+      )
+    } ~
+      path("health") {
         complete(
           StatusCodes.OK,
           HttpEntity(
             ContentTypes.`application/json`,
-            s"""{"message": "Hello $name"}""".stripMargin
+            """{"status": "OK"}"""
           )
         )
-      } ~
-        path("health") {
-          complete(
-            StatusCodes.OK,
-            HttpEntity(
-              ContentTypes.`application/json`,
-              """{"status": "OK"}"""
-            )
-          )
-        }
+      }
 
+  def main(args: Array[String]): Unit = {
     Http().bindAndHandle(welcomeRoute, "0.0.0.0", 8080)
   }
 }
